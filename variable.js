c08a8c9164b20fce31a1f7948b2dfd5a2737ff7a
Ebru <yerdebru@gmail.com>
2023-08-14 19:06:02 +0300

    change

--- variable.js
+++ variable.js
@@ -26,7 +26,7 @@ document.getElementById("mysubmit").onclick = function () {
   document.getElementById("myp").innerHTML = Input;
 };
 
-let pi = 3.14;
+/*let pi = 3.14;
 let radius;
 let circumference;
 
@@ -34,4 +34,33 @@ radius = window.prompt("Yo enter ur radius here");
 radius = Number(radius);
 
 circumference = 2 * pi * radius;
-console.log("ur circumference is", circumference);
+console.log("ur circumference is", circumference);*/
+
+//challengetwo
+document.getElementById("submit1").onclick = function () {
+  ourtext = document.getElementById("text1").value;
+  document.getElementById("psubmit1").innerHTML = ourtext;
+};
+
+document.getElementById("length").onclick = function () {
+  ourtext1 = document.getElementById("text1").value;
+  ourtext1 = ourtext1.length;
+  document.getElementById("pall").innerHTML = "String length: " + ourtext1;
+};
+
+document.getElementById("Uppercase").onclick = function () {
+  ourtext2 = document.getElementById("text1").value;
+  ourtext2 = ourtext2.toUpperCase();
+  document.getElementById("pall").innerHTML = ourtext2;
+};
+
+document.getElementById("Lowercase").onclick = function () {
+  ourtext3 = document.getElementById("text1").value;
+  ourtext3 = ourtext2.toLowerCase();
+  document.getElementById("pall").innerHTML = ourtext3;
+};
+
+document.getElementById("Clear").onclick = function () {
+  document.getElementById("pall").remove();
+  document.getElementById("psubmit1").remove();
+};
